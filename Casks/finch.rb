3000e5a329a49e1ce1f7d7c0ab239907902b09ed
Anqi Pang <anqipans@amazon.com>
2022-12-07 13:59:24 -0800

    update v0.1.1 and uninstall script

--- Casks/finch.rb
+++ Casks/finch.rb
@@ -5,19 +5,17 @@ cask "finch" do
   sha256 arm:   "0274a41c2c86b6e3eb762b16223869fdeec7811c4c3e7703ce2b8888cb61976c",
          intel: "e4c8f7c944c75c5b748c54ab58e1bc73337c22bbc82f94d75171533a6dddf550"
 
-  url "file:///Users/ec2-user/Finch-v#{version}-#{arch}.pkg"
+  url "https://github.com/runfinch/finch/releases/download/v#{version}/Finch-v#{version}-#{arch}.pkg"
   name "Finch"
   desc "Open source container development tool"
   homepage "https://github.com/runfinch/finch"
 
   pkg "Finch-v#{version}-#{arch}.pkg"
 
-  uninstall delete:  [
-              "/opt/finch/",
-              "/private/var/run/finch-lima",
-              "/private/etc/sudoers.d/finch-lima",
-            ],
-            pkgutil: "org.Finch.v#{version}"
+  uninstall script: {
+    executable: "/Applications/Finch/uninstall.sh",
+    sudo:       true,
+  }
 
   zap trash: "~/.finch"
 end
